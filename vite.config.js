08e0d075ecf82dcf4ede017ee88de355319cb294
Meet-Maheshwari <meetmaheshwari2107@gmail.com>
2025-04-04 22:20:43 +0530

    Added changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "https://auth-backend-vert-six.vercel.app",
     },
   },
   plugins: [react()],
